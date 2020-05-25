bdf6cd36fb62ab560e4282d90ce82407cfd8543a
DosImpact <ypd03008@gmail.com>
2020-05-25 15:27:46 +0900

    env

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 5000;
 
-const CONNECT_URL =
-  "mongodb+srv://dosimpact:589742@shoppingmall-5jqmh.mongodb.net/test?retryWrites=true&w=majority";
+const config = require("./config/key");
 
 const mongoose = require("mongoose");
+const User = require("./models/User");
+
 mongoose
-  .connect(CONNECT_URL, {
+  .connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: true,
   })
-  .then(() => console.log("mongoDB is connected! ✅"))
+  .then(() => console.log("mongoDB is connected! ✅"))
   .catch((e) => console.error(e));
 
-app.get("/", (req, res) => res.send("hello world"));
+// url 자체 해석 및 json 형태 해석
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("hello world"));
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  User.create(req.body)
+    .then(() => {
+      return res.status(200).json({
+        success: true,
+      });
+    })
+    .catch(() => {
+      return res.json({ success: false, err });
+    });
+});
 app.listen(port, () =>
-  console.log(`Server is running at http://localhost:${port} ✅`)
+  console.log(`Server is running at http://localhost:${port} ✅`)
 );
